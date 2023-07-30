c5f074d193386762aaab8717137b511fc893d999
benmelz <ben@melz.me>
2023-07-30 11:04:10 -0400

    configure rspec, add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-require 'omniauth/strategies/passthrough'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter %r{^/spec/}
+  enable_coverage :branch
+  minimum_coverage line: 100, branch: 100
+end
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
